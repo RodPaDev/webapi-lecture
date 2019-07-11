f46a5667ce974fb50c5346b91bd6b20d4508a1c3
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-07-11 11:32:43 +0200

    wip

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+server.listen(PORT, () => {
   console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
